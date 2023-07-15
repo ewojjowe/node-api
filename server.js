dc9af48698b72786f6c7747eeab0b84e3a36cea9
John Wilfred Montilde <wilfredjohnmontilde19@gmail.com>
2023-07-15 23:53:45 +0800

    add custom error middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const productRoute = require('./routes/productRoute')
+const errorMiddleware = require('./middlewares/errorMiddleware')
 
 const app = express()
 
@@ -14,14 +15,17 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/products', productRoute)
 
 app.get('/', (req, res) => {
+  //   throw new Error('fake error')
   res.send('Node API')
 })
+
+app.use(errorMiddleware)
 
 mongoose.set('strictQuery', false)
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    console.log('Connected to mongoDB')
+    console.log('Connected to mongoDB!')
     app.listen(PORT, () => {
       console.log(`Node server running port ${PORT}`)
     })
