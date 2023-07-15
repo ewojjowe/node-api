734c6d83ca16182b4c7f2081682aed87086e49fa
John Wilfred Montilde <wilfredjohnmontilde19@gmail.com>
2023-07-16 00:25:03 +0800

    handle cors issue

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const productRoute = require('./routes/productRoute')
 const errorMiddleware = require('./middlewares/errorMiddleware')
+const cors = require('cors')
 
 const app = express()
 
-const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL
+const ALLOWED_ORIGIN_1 = process.env.ALLOWED_ORIGIN_1
+const ALLOWED_ORIGIN_2 = process.env.ALLOWED_ORIGIN_2
 
+const corsOptions = {
+  origin: [ALLOWED_ORIGIN_1, ALLOWED_ORIGIN_2], // change to allowed IP or Domain
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(errorMiddleware)
 
 app.use('/api/products', productRoute)
 
@@ -18,8 +28,6 @@ app.get('/', (req, res) => {
   //   throw new Error('fake error')
   res.send('Node API')
 })
-
-app.use(errorMiddleware)
 
 mongoose.set('strictQuery', false)
 mongoose
